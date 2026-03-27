93a8a3f048ff0e8729b515a67597762d21cf336f
coderpwh1024 <pengwenhao@aliyun.com>
2026-03-27 18:00:59 +0800

    fix:update HookStopAgentExample

--- quickstart/src/main/java/com/coderpwh/agentscope/example/HookStopAgentExample.java
+++ quickstart/src/main/java/com/coderpwh/agentscope/example/HookStopAgentExample.java
@@ -1,6 +1,11 @@
 package com.coderpwh.agentscope.example;
 
+import java.awt.*;
 import java.io.IOException;
+
+import io.agentscope.core.tool.Tool;
+import io.agentscope.core.tool.ToolParam;
+import io.agentscope.core.tool.Toolkit;
 
 /**
  * @author coderpwh
@@ -15,10 +20,19 @@ public class HookStopAgentExample {
                         + "代理在执行敏感操作前会暂停，\n"
                         + "让您可以查看并确认工具调用。");
 
-        String  apiKey = ExampleUtils.getDashScopeApiKey();
+        String apiKey = ExampleUtils.getDashScopeApiKey();
+
+        Toolkit toolkit = new Toolkit();
 
 
+    }
 
+    public static class SensitiveTools {
+        @Tool(name = "deleteFile", description = "Delete a file from the system")
+        public String deleteFile(@ToolParam(name = "filename", description = "Name of the file to delete") String filename) {
+            System.out.println("工具 删除文件:"+filename);
+            return "File '" + filename + "' has been deleted successfully.";
+        }
 
     }
 
