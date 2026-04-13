a71b1826238d9d1ef016c7c23a51ddb0ce8f752d
pengwenhao <pengwenhao@aliyun.com>
2026-04-13 17:20:19 +0800

    feat:init chat tts

---
+++ chat-tts/chat-tts/src/main/java/com/coderpwh/chattts/ChatTTSSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.coderpwh.chattts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author coderpwh
+ */
+@SpringBootApplication
+public class ChatTTSSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatTTSSpringBootApplication.class, args);
+    }
+}
