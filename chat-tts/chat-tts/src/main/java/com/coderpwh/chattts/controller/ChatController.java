9f79d70af6036330a914363ce8b89c19d6aab574
pengwenhao <pengwenhao@aliyun.com>
2026-04-14 15:34:36 +0800

    fix:update ChatController

--- chat-tts/chat-tts/src/main/java/com/coderpwh/chattts/controller/ChatController.java
+++ chat-tts/chat-tts/src/main/java/com/coderpwh/chattts/controller/ChatController.java
@@ -1,18 +1,26 @@
 package com.coderpwh.chattts.controller;
 
+import io.agentscope.core.ReActAgent;
+import io.agentscope.core.agent.EventType;
+import io.agentscope.core.agent.StreamOptions;
 import io.agentscope.core.hook.TTSHook;
 import io.agentscope.core.message.Base64Source;
+import io.agentscope.core.message.Msg;
+import io.agentscope.core.message.MsgRole;
+import io.agentscope.core.message.TextBlock;
 import io.agentscope.core.model.DashScopeChatModel;
 import io.agentscope.core.model.tts.DashScopeRealtimeTTSModel;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.scheduling.annotation.Schedules;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.Map;
 
@@ -60,25 +68,78 @@ public class ChatController {
                 .build();
 
 
-         //  创建TTSHook
-        TTSHook  ttsHook = TTSHook.builder()
+        //  创建TTSHook
+        TTSHook ttsHook = TTSHook.builder()
                 .ttsModel(requestTtsModel)
-                .audioCallback(audio->{
-                   if(audio.getSource() instanceof Base64Source src ){
-                       sink.tryEmitNext(
-                               ServerSentEvent.<Map<String,Object>>builder()
-                                       .event("audio")
-                                       .data(Map.of("audio",src.getData())).build()
-                       );
-                   }
+                .audioCallback(audio -> {
+                    if (audio.getSource() instanceof Base64Source src) {
+                        sink.tryEmitNext(
+                                ServerSentEvent.<Map<String, Object>>builder()
+                                        .event("audio")
+                                        .data(Map.of("audio", src.getData())).build()
+                        );
+                    }
                 }).build();
 
 
+        ReActAgent agent = ReActAgent.builder()
+                .name("Assistant")
+                .sysPrompt("你是一个友好的中文助手.请用简洁的中文回答问题")
+                .model(chatModel)
+                .hook(ttsHook)
+                .maxIters(3)
+                .build();
 
 
+        Msg userMsg = Msg.builder()
+                .name("user")
+                .role(MsgRole.USER)
+                .content(TextBlock.builder().text(messages).build())
+                .build();
 
 
-        return null;
+        agent.stream(userMsg, StreamOptions.builder().eventTypes(EventType.REASONING).incremental(true).build())
+                .subscribeOn(Schedulers.boundedElastic())
+                .doOnNext(evnet -> {
+                    String text = evnet.getMessage().getTextContent();
+                    if (text != null && !text.isEmpty()) {
+                        sink.tryEmitNext(ServerSentEvent.<Map<String, Object>>builder()
+                                .event("text")
+                                .data(Map.of("text", "text", "isLast", evnet.isLast())).build());
+                    }
+                })
+                .doOnComplete(() -> {
+                    ttsHook.stop();
+                    requestTtsModel.close();
+                    sink.tryEmitNext(
+                            ServerSentEvent.<Map<String, Object>>builder()
+                                    .event("done")
+                                    .data(Map.of("status", "completed"))
+                                    .build());
+                    sink.tryEmitComplete();
+                })
+                .doOnError(e -> {
+                    // Stop TTS hook and close WebSocket session on error
+                    ttsHook.stop();
+                    requestTtsModel.close();
+
+                    sink.tryEmitNext(
+                            ServerSentEvent.<Map<String, Object>>builder()
+                                    .event("error")
+                                    .data(Map.of("error", e.getMessage()))
+                                    .build());
+                    sink.tryEmitComplete();
+                })
+                .subscribe();
+
+         // 返回SSE流
+        return sink.asFlux().doOnCancel(
+                () -> {
+                    ttsHook.stop();
+                    requestTtsModel.close();
+                    sink.tryEmitComplete();
+                });
+
     }
 
 
