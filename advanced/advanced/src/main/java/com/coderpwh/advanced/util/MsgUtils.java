f89cd33129694f5afb0fc4a93fa1c20402fe19ca
pengwenhao <pengwenhao@aliyun.com>
2026-04-15 15:48:24 +0800

    feat:init msgUtils

---
+++ advanced/advanced/src/main/java/com/coderpwh/advanced/util/MsgUtils.java
@@ -0,0 +1,42 @@
+package com.coderpwh.advanced.util;
+
+import com.google.gson.internal.bind.SerializationDelegatingTypeAdapter;
+import io.agentscope.core.message.ContentBlock;
+import io.agentscope.core.message.Msg;
+import io.agentscope.core.message.TextBlock;
+
+import java.util.List;
+
+public final class MsgUtils {
+
+
+    private MsgUtils() {
+
+    }
+
+    public static String getTextContent(Msg msg) {
+
+        if (msg == null) {
+            return "";
+        }
+        List<ContentBlock> content = msg.getContent();
+        if (content == null || content.isEmpty()) {
+            return "";
+        }
+
+
+        StringBuilder sb = new StringBuilder();
+        for (ContentBlock block : content) {
+            if (block instanceof TextBlock textBlock) {
+                String text = textBlock.getText();
+                if (text != null) {
+                    sb.append(text);
+                }
+            }
+        }
+        return sb.toString();
+
+    }
+
+
+}
