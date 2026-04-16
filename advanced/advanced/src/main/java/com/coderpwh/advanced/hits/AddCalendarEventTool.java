ba4597c383509a226c51bc3fa9cc4c42b102cd0b
pengwenhao <pengwenhao@aliyun.com>
2026-04-16 14:40:48 +0800

    fix:update AddCalendarEvent

--- advanced/advanced/src/main/java/com/coderpwh/advanced/hits/AddCalendarEventTool.java
+++ advanced/advanced/src/main/java/com/coderpwh/advanced/hits/AddCalendarEventTool.java
@@ -1,6 +1,7 @@
 package com.coderpwh.advanced.hits;
 
 import io.agentscope.core.tool.Tool;
+import io.agentscope.core.tool.ToolParam;
 
 /**
  * @author coderpwh
@@ -8,7 +9,7 @@ import io.agentscope.core.tool.Tool;
 public class AddCalendarEventTool {
 
 
-    public static  final String TOOL_NAME = "add_calendar_event";
+    public static final String TOOL_NAME = "add_calendar_event";
 
 
     @Tool(
@@ -18,9 +19,9 @@ public class AddCalendarEventTool {
                             + " workout separately. For example, if the plan has workouts on Monday,"
                             + " Tuesday, and Wednesday, call this tool 3 times with each day's"
                             + " details.")
-    public String addCalendarEvent(){
+    public String addCalendarEvent(@ToolParam(name = "title", description = "Event title,e.g 'Chest + Triceps Workout' ") String title) {
 
-        return  null;
+        return null;
 
     }
 
