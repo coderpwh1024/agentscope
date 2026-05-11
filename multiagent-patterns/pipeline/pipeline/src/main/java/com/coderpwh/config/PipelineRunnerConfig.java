1c0db4d5c6528eef39cb81efeeabb1bb0d5640f6
pengwenhao <pengwenhao@aliyun.com>
2026-05-11 14:59:34 +0800

    feat:init PipelineRunnerConfig

---
+++ multiagent-patterns/pipeline/pipeline/src/main/java/com/coderpwh/config/PipelineRunnerConfig.java
@@ -0,0 +1,14 @@
+package com.coderpwh.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author coderpwh
+ */
+@Configuration
+public class PipelineRunnerConfig {
+
+
+
+
+}
