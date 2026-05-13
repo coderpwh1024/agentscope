7eb5c5cfe32cebc7f95e24ff042600622c4358f4
coderpwh <pengwenhao@aliyun.com>
2026-05-13 09:46:34 +0800

    feat:init routing

---
+++ routing/routing/src/main/java/com/coderpwh/routing/RoutingApplication.java
@@ -0,0 +1,13 @@
+package com.coderpwh.routing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutingApplication.class, args);
+    }
+
+}
