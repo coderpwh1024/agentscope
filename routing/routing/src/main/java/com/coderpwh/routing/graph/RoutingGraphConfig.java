ba9a14934198db74e28020e2cd82bb8b17d2771c
coderpwh <pengwenhao@aliyun.com>
2026-05-13 14:01:06 +0800

    fix:update RoutingGraphConfig

--- routing/routing/src/main/java/com/coderpwh/routing/graph/RoutingGraphConfig.java
+++ routing/routing/src/main/java/com/coderpwh/routing/graph/RoutingGraphConfig.java
@@ -1,6 +1,16 @@
 package com.coderpwh.routing.graph;
 
 import com.alibaba.cloud.ai.agent.agentscope.AgentScopeAgent;
+import com.alibaba.cloud.ai.agent.agentscope.flow.AgentScopeRoutingAgent;
+import com.alibaba.cloud.ai.graph.CompiledGraph;
+import com.alibaba.cloud.ai.graph.KeyStrategy;
+import com.alibaba.cloud.ai.graph.KeyStrategyFactory;
+import com.alibaba.cloud.ai.graph.StateGraph;
+import com.alibaba.cloud.ai.graph.exception.GraphStateException;
+import com.alibaba.cloud.ai.graph.state.strategy.AppendStrategy;
+import com.alibaba.cloud.ai.graph.state.strategy.ReplaceStrategy;
+import com.coderpwh.routing.graph.node.PostprocessNode;
+import com.coderpwh.routing.graph.node.PreprocessNode;
 import com.coderpwh.routing.graph.tools.GitHubStubTools;
 import com.coderpwh.routing.graph.tools.NotionStubTools;
 import com.coderpwh.routing.graph.tools.SlackStubTools;
@@ -11,6 +21,13 @@ import io.agentscope.core.model.Model;
 import io.agentscope.core.tool.Toolkit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.alibaba.cloud.ai.graph.StateGraph.END;
+import static com.alibaba.cloud.ai.graph.action.AsyncNodeAction.node_async;
 
 @Configuration
 public class RoutingGraphConfig {
@@ -127,6 +144,64 @@ public class RoutingGraphConfig {
                 .instruction("Please respond to the following request: {slack_input}.")
                 .outputKey("slack_key")
                 .build();
+    }
+
+
+    /**
+     * 路由
+     *
+     * @param githubAgent
+     * @param notionAgent
+     * @param slackAgent
+     * @return
+     */
+    @Bean
+    public AgentScopeRoutingAgent routingAgent(AgentScopeAgent githubAgent, AgentScopeAgent notionAgent, AgentScopeAgent slackAgent) {
+        return AgentScopeRoutingAgent.builder()
+                .name("router")
+                .model(dashScopeModel())
+                .description("Routes queries to GitHub, Notion, and/or Slack specialists based on"
+                        + " relevance.")
+                .subAgents(List.of(githubAgent, notionAgent, slackAgent))
+                .build();
+    }
+
+
+    /***
+     * 路由图
+     * @param routingAgent
+     * @return
+     * @throws GraphStateException
+     */
+    @Bean
+    public CompiledGraph routingGraph(AgentScopeRoutingAgent routingAgent) throws GraphStateException {
+
+        KeyStrategyFactory keyFactory = () -> {
+            Map<String, KeyStrategy> strategies = new HashMap<>();
+            strategies.put("input", new ReplaceStrategy());
+            strategies.put("query", new ReplaceStrategy());
+            strategies.put("messages", new AppendStrategy(false));
+            strategies.put("preprocess_metadata", new ReplaceStrategy());
+            strategies.put("merged_result", new ReplaceStrategy());
+            strategies.put("final_answer", new ReplaceStrategy());
+            strategies.put("postprocess_metadata", new ReplaceStrategy());
+            strategies.put("github_key", new ReplaceStrategy());
+            strategies.put("notion_key", new ReplaceStrategy());
+            strategies.put("slack_key", new ReplaceStrategy());
+            return strategies;
+
+        };
+
+        StateGraph graph = new StateGraph("routing_graph", keyFactory)
+                .addNode("preprocess", node_async(new PreprocessNode()))
+                .addNode("routing", routingAgent.getAndCompileGraph())
+                .addNode("postprocess", node_async(new PostprocessNode()))
+                .addEdge("START", "preprocess")
+                .addEdge("preprocess", "routing")
+                .addEdge("routing", "postprocess")
+                .addEdge("postproces", END);
+
+        return graph.compile();
     }
 
 
