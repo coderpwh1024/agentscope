72a5272a3efde651ea2c40075f94e196b6877390
pengwenhao <pengwenhao@aliyun.com>
2026-05-16 17:17:54 +0800

    fix:update RoutingService

--- multiagent-patterns/routing/routing/src/main/java/com/coderpwh/routing/simple/RouterService.java
+++ multiagent-patterns/routing/routing/src/main/java/com/coderpwh/routing/simple/RouterService.java
@@ -1,9 +1,14 @@
 package com.coderpwh.routing.simple;
 
 import com.alibaba.cloud.ai.agent.agentscope.flow.AgentScopeRoutingAgent;
+import com.coderpwh.routing.simple.state.AgentOutput;
+import com.coderpwh.routing.simple.state.Classification;
 import io.agentscope.core.model.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.messages.Message;
+
+import java.util.List;
 
 /**
  * @author coderpwh
@@ -17,7 +22,7 @@ public class RouterService {
     private static final String SYNTHESIZE_SYSTEM_TEMPLATE =
             """
                     Synthesize these search results to answer the original question: "%s"
-                    
+                                        
                     - Combine information from multiple sources without redundancy
                     - Highlight the most relevant and actionable information
                     - Note any discrepancies between sources
@@ -39,6 +44,35 @@ public class RouterService {
 
 
 
+   private static  String extraText(Object output){
+        if(output instanceof Message message){
+            return  message.getText();
+        }
+
+        return  output!=null?output.toString():"";
+   }
+
+    private static String capitalize(String s) {
+        if (s == null || s.isEmpty()) {
+            return s;
+        }
+        return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
+    }
+
+
+    /**
+     * 路由返回结果
+     *
+     * @param query
+     * @param classifications
+     * @param results
+     * @param finalAnswer
+     */
+
+    public record RouteResult(String query, List<Classification> classifications, List<AgentOutput> results,
+                              String finalAnswer) {
+
+    }
 
 
 }
